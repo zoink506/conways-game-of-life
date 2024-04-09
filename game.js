48abed194317a951513297d0bd9df8ae839e94a9
zoink506 <benhubble03@gmail.com>
2024-04-09 17:00:54 +1000

    Print game board to the screen

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 function startGame() {
-  const boardWidth = 16;
-  const boardHeight = 16;
+  const boardWidth = 32;
+  const boardHeight = 32;
 
   const gameBoard = [];
   for(let i = 0; i < boardWidth; i++) {
@@ -9,7 +9,7 @@ function startGame() {
     for(let j = 0; j < boardHeight; j++) {
       // Start with randomly chosen cell state
       // Change to user input later
-      if(Math.random() >= 0.3) {
+      if(Math.random() >= 0.4) {
         gameBoard[i][j] = "X";
       } else {
         gameBoard[i][j] = "O";
@@ -18,6 +18,35 @@ function startGame() {
   }
 
   console.log(gameBoard);
+  displayBoard(gameBoard);
+}
+
+function displayBoard(board) {
+  const gameBoardElement = document.getElementById("game-board");
+  gameBoardElement.innerHTML = "";
+  
+  for(let i = 0; i < board.length; i++) {
+    const newRow = document.createElement("div");
+    newRow.classList.add("row");
+    newRow.id = `row-${i}`;
+
+    for(let j = 0; j < board[i].length; j++) {
+      const newCell = document.createElement("div");
+      newCell.classList.add("cell");
+      newCell.id = `row-${i}-column-${j}`;
+
+      if(board[i][j] == "X") {
+        newCell.classList.add("active");
+      } else {
+        newCell.classList.add("disactive");
+      }
+
+      newRow.append(newCell);
+    }
+
+    // place newRow into #game-board div
+    gameBoardElement.append(newRow);
+  }
 }
 
 startGame();
